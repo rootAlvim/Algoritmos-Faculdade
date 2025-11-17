604a1ad0c8ca93bb9da4049fcb9eb6fafe73367a
Francisco Álvaro Chaves da Silva <alvaro.chaves@escolar.ifrn.edu.br>
2025-11-17 07:31:03 -0300

    update

---
+++ kara.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+using namespace std;
+long long karatsuba(long x, long y)
+{
+   if (x < 10 || y < 10) // caso base
+        return x * y;
+
+    // transforma em string pra contar os dígitos
+    string X = to_string(x);
+    string Y = to_string(y);
+    int n = max(X.length(), Y.length());
+    int m = n / 2;
+    return n;
+}
+int main()
+{
+    cout << "Valor " << karatsuba(12000000000000000, 9) << endl;
+}
